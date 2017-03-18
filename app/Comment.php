de4da1490d0df259c7d896365fbcb4c4f2eb5b54
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-03-18 05:19:03 +0300

    Implement top level comments

--- app/Comment.php
+++ app/Comment.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    protected $fillable = ['user_id', 'body'];
+    
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,23 @@
 <?php
 
+// Temporary
+Auth::loginUsingId(1);
+
 Route::get('/', function () {
     $posts = App\Post::all();
     return view('posts.index')->withPosts($posts);
 });
+
 Route::get('/posts/{post}', function (App\Post $post) {
     return view('posts.show')->withPost($post);
 });
+
+// /posts/{{ $post->id }}/comments
+Route::post('/posts/{post}/comments', function (App\Post $post) {
+    $post->comments()->create([
+        'user_id' => Auth::id(),
+        'body' => request('body')
+    ]);
+
+    return back();
+})->middleware('auth');
